09b60dffc109faacd82083a21f4be2019d657b84
Norberto Esteves <norbertoesteves@hotmail.com>
2014-08-23 03:58:23 +0000

    Strings checkpoint

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,23 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName + ",\n\n" +
+  message + "\n\n" + 
+  "Sincerely,\n" + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+ return bigString.substring(startA, endA) + bigString.substring(startB, endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  var first = text.indexOf(",");
+  var second = text.lastIndexOf(searchString);
+  return text.substring(first, second);
 };
