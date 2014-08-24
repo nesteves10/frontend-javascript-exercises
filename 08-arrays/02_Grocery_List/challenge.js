bd61ad474ad6ee77347291ae90bb0e5c4f85d64c
Norberto Esteves <norbertoesteves@hotmail.com>
2014-08-24 22:17:24 +0000

    arrays checkpoint

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,13 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, myArray){
+  if(myArray.indexOf(item) === -1){
+  myArray.push(item);
+  console.log(myArray);
+  
+}
+return myArray;
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(myArray){
+  myArray.sort();
+  return myArray.reverse();
+};
